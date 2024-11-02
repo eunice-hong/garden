ad0526d923dd31ca0be2b382fbc76b4c2e4fd472
Eunice Hong <dev@eunice-hong.com>
2024-11-02 15:46:32 +0900

    feat: use KR fonts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Hahmlet",
+        body: "Gothic A1",
+        code: "IBM Plex Mono KR",
       },
       colors: {
         lightMode: {
