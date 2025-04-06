d0bcf842ea12ef8d046b36d2cb2ef7b9a2aa3b41
Eunice Hong <dev@eunice-hong.com>
2025-04-06 12:06:14 +0900

    feat: add mobile-only Table of Contents to default content page layout
    - Included the TableOfContents component wrapped in MobileOnly for improved mobile user experience.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -20,6 +20,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
+    Component.MobileOnly(Component.TableOfContents()),
   ],
   afterBody: [
     Component.Comments({
